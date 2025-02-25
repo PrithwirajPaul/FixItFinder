b6de629c4eedff93cd61f21c4c5c283b1398617c
Naeem Beg <mirzanaeem278@gmail.com>
2025-02-26 01:07:23 +0600

    Create OffAccountController.cs

---
+++ Controllers/OffAccountController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FixItFinderDemo.Controllers
+{
+    public class OffAccountController : Controller
+    {
+        public IActionResult OfferService()
+        {
+            return View();
+        }
+        public IActionResult AssemblyOPage()
+        {
+            return View();
+        }
+        public IActionResult MountingOPage()
+        {
+            return View();
+        }
+        public IActionResult CleaningOPage()
+        {
+            return View();
+        }
+        public IActionResult RepairOPage()
+        {
+            return View();
+        }
+        public IActionResult ElectricOPage()
+        {
+            return View();
+        }
+        public IActionResult PlumbingOPage()
+        {
+            return View();
+        }
+        public IActionResult PainterOPage()
+        {
+            return View();
+        }
+        public IActionResult CarpenterOPage()
+        {
+            return View();
+        }
+    }
+}
