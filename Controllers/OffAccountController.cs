316d6e2b69d0bedc1fe2785b8767b41732c0cb71
Naeem Beg <mirzanaeem278@gmail.com>
2025-03-06 00:21:15 +0600

    Update OffAccountController.cs

--- Controllers/OffAccountController.cs
+++ Controllers/OffAccountController.cs
@@ -1,44 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using FixItFinderDemo.Data;
+using FixItFinderDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FixItFinderDemo.Controllers
 {
-    public class OffAccountController : Controller
+    public class OffAccountController(FIFContext context) : Controller
     {
+        private readonly FIFContext _context = context;
+
+        
+        private List<Post> FetchCustomerPosts(string category)
+        {
+            return _context.Posts
+                .Include(p => p.User)
+                .ThenInclude(u => u.Customer_Profile)
+                .Where(p => p.User != null
+                         && p.User.Role == "Customer"
+                         && p.User.Customer_Profile != null
+                         && p.User.Customer_Profile.Category == category)
+                .ToList();
+        }
+
         public IActionResult OfferService()
         {
             return View();
         }
-        public IActionResult AssemblyOPage()
+
+        public IActionResult ServiceOfferPage(string category)
         {
-            return View();
+            int? userId = HttpContext.Session.GetInt32("UserId");
+            string? role = HttpContext.Session.GetString("UserRole");
+
+            var customerPosts = FetchCustomerPosts(category);
+
+            ViewBag.UserId = userId;
+            ViewBag.Role = role;
+
+            return View(customerPosts);
         }
-        public IActionResult MountingOPage()
-        {
-            return View();
-        }
-        public IActionResult CleaningOPage()
-        {
-            return View();
-        }
-        public IActionResult HouseRepairOPage()
-        {
-            return View();
-        }
-        public IActionResult ElectritiansOPage()
-        {
-            return View();
-        }
-        public IActionResult PlumbingOPage()
-        {
-            return View();
-        }
-        public IActionResult PainterOPage()
-        {
-            return View();
-        }
-        public IActionResult CarpenterOPage()
-        {
-            return View();
-        }
-    }
-}
